23609a384fa0efc05e51e60291eca666b79ff8d5
Richard Kallos <rkallos@gmail.com>
2020-10-25 15:01:22 -0400

    Implement #average sampling rule

--- test/drip_test.erl
+++ test/drip_test.erl
@@ -5,7 +5,8 @@
 drip_test_() ->
     {setup, fun() -> application:ensure_all_started(drip) end, fun(_) -> application:stop(drip) end,
         [
-            fun static_rules/0
+            fun static_rules/0,
+            fun calculate_new_sample_rates/0
         ]}.
 
 static_rules() ->
@@ -18,3 +19,10 @@ static_rules() ->
     ?assertEqual(drip:sample(not_a_key), false),
     ?assertEqual(drip:any([foo, bar, baz]), true),
     ?assertEqual(drip:all([foo, bar, baz]), false).
+
+calculate_new_sample_rates() ->
+    Desired = 10,
+    Samples = [{a, 100}, {b, 10}, {c, 2}],
+    Expected = #{a => 15, b => 3, c => 2},
+    Actual = drip_server:calculate_new_sample_rates(Samples, Desired),
+    ?assertEqual(Actual, Expected).
